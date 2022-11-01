76b530260d0bfcca6ea03ed0dcb94e90785e3e6e
cy-yu-1216 <1243768319@qq.com>
2022-11-01 16:41:59 +0800

    引入iconfont图标、封装图标使用

---
+++ src/base-ui/icon-svg/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use :xlink:href="icon"></use>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { computed, toRefs } from 'vue'
+
+const props = defineProps({
+  iconName: {
+    type: String,
+    default: '#icon-yonghuming'
+  }
+})
+const propsRefs = toRefs(props)
+const icon = computed(() => `#icon-${propsRefs.iconName.value}`)
+</script>
+
+<style scoped lang="scss"></style>
+s
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,19 @@ import router from './router'
 import pinia from './store'
 import { registerApp } from './global'
 import './permission'
+// import { ElMessage } from 'element-plus'
+import iconSvg from './base-ui/icon-svg/index.vue'
 
 const app = createApp(App)
+
+// const ElMessageCfg = { duration: 2000, customClass: 'globalElMessageStyle' }
+// app.config.globalProperties.$ElMessage = (msg: string) => {
+//   return ElMessage.success({ message: msg, ...ElMessageCfg })
+// }
 
 app.use(registerApp)
 app.use(router)
 app.use(pinia)
+
+app.component('icon-svg', iconSvg)
 app.mount('#app')
