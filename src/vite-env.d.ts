0abec2377ec60344ca9eb9683dd092d5446fb7c3
cy-yu-1216 <1243768319@qq.com>
2022-11-01 16:43:12 +0800

    安装进度条、在中permission中使用

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,7 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare module 'nprogress' {
+  const content: any
+  export default content
+}
